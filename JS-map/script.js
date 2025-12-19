e3b5d52391a25118d01c315522aabc82e6f2076c
JelleSchrijvers-PXL <12504040@student.pxl.be>
2025-12-19 16:39:20 +0100

    WEB-13: werkende dropdown sidebar

--- JS-map/script.js
+++ JS-map/script.js
@@ -10,3 +10,25 @@ function ToonMenu() {
         menuIcon.src = "/JS-map/iconen/close_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"
     }
 }
+
+function ToonSideBar() {
+    const SideBar = document.querySelector(".over-mij");
+    const Contact = document.querySelector(".contact-info");
+    const Socials = document.querySelector(".socials");
+    const SideBarLength = document.querySelector(".sidebar");
+    const dropdownIcon = document.querySelector(".dropdown-icon");
+
+    if (SideBar.style.display === "block"){
+        SideBar.style.display = "none";
+        Contact.style.display = "none";
+        Socials.style.display = "none";
+        SideBarLength.style.height = "6rem";
+        dropdownIcon.src ="/JS-map/iconen/chevron-down-svgrepo-com.svg";
+    }else {
+        SideBar.style.display = "block";
+        Contact.style.display = "block";
+        Socials.style.display = "block";
+        SideBarLength.style.height = "32rem";
+        dropdownIcon.src = "/JS-map/iconen/chevron-up-svgrepo-com.svg";
+    }
+}
